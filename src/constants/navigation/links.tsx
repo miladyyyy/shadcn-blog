b3c7023e94b7f6e7e45ad05539fe6e8e6a50d2ae
Anirudh Sriram <techwithanirudh@gmail.com>
2026-01-24 18:14:46 +0000

    refactor: reorder link items for consistency in navigation

--- src/constants/navigation/links.tsx
+++ src/constants/navigation/links.tsx
@@ -3,14 +3,14 @@ import { Icons } from '@/components/icons/icons'
 
 export const linkItems: LinkItemType[] = [
   {
+    text: 'About',
+    icon: <Icons.user />,
+    url: '/about',
+  },
+  {
     text: 'Posts',
     icon: <Icons.posts />,
     url: '/posts',
     active: 'nested-url',
-  },
-  {
-    text: 'About',
-    icon: <Icons.user />,
-    url: '/about',
   },
 ]
